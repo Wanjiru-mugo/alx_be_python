7736c9ae7b9a3da5d5e30d66a9bee3a661f1e3e3
Wanjiru-mugo <wanjirumugomary@gmail.com>
2025-06-02 02:30:50 +0300

    basic arithmetic exercise

---
+++ basic_operations.py
@@ -0,0 +1,11 @@
+#program calculates the sum, difference, and product of 2 variables
+number1 = 10
+number2 = 5
+sum = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+
+#now we print their results 
+print("Addition of", number1, "and", number2, "is", sum)
+print("Subtraction of", number1, "and", number2, "is", different)
+print("Multiplication of", number1 "and", number2, "is", product)
