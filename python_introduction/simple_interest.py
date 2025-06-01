1b2d62d8557551a2f6a4cd16dbdea856de77d5c1
Wanjiru-mugo <wanjirumugomary@gmail.com>
2025-06-02 02:50:16 +0300

    simple interest calculator

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,9 @@
+#program calculates simple interest earned
+principal = 1000
+rate = 0.05
+time = 3
+
+#defining simple interest (I) formula
+I = principal * rate * time
+
+print("The simple interest is:", I)
