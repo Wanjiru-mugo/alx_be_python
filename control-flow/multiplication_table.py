418756d122168d6903a0cc975300f3d3f2c97a7b
Wanjiru-mugo <wanjirumugomary@gmail.com>
2025-07-15 07:37:03 +0300

    multiplication table

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,6 @@
+#program asks user for random number and creates a multiplication table for that number running from 1-10
+number = int(input('Enter a number to see its multiplication table: '))
+i = 1
+for i in range(1, 11):
+    product = number * i
+    print(f'{number} * {i} = {product}')
