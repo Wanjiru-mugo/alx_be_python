cf1589545246fddc9afd68130517ea3353b54ca9
Wanjiru-mugo <wanjirumugomary@gmail.com>
2025-08-05 02:30:34 +0300

    arithmetic operations

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,13 @@
+def perform_operation(num1, num2, operation):
+
+    if operation == 'divide':
+        if num 2 == 0:
+            print('undefined')
+        else:
+            return num1 /num2
+    elif operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    else:
+        return num1 * num2
