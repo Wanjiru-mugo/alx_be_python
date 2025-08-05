b53b4c1a0e0d36c70801e0bd681f43ca91cb16a0
Wanjiru-mugo <wanjirumugomary@gmail.com>
2025-08-06 02:51:53 +0300

    list operations

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,21 +1,28 @@
+print('Shopping List Manager')
+
 shopping_list = list() #declaring empty list; also shopping_list []
+
 def display_menu():
+    print('1. Add Item')
+    print('2. Remove Item')
+    print('3. View List')
+    print('4. Exit')
 
     while True:
-        item = input('please choose option (add, remove, view, exit) to the list:').strip().lower()
+        item = input('Enter your choice (1-4): ').strip()
 
-        if item == 'add':
+        if item == '1':
             item = input('Add an item to the shopping list:').strip().lower()
             shopping_list.append(item)
-        elif item == 'remove':
+        elif item == '2':
             item = input('Choose an item to remove from shopping list:').strip().lower()
             if not item in shopping_list:
                 print(f'Item not found')
             else:
                 shopping_list.remove(item)
-        elif item == 'view':
+        elif item == '3':
             print(shopping_list)
-        elif item == 'exit':
+        elif item == '4':
             print(f'Goodbye')
         else:
             print(f'Invalid choice. Please try again')
