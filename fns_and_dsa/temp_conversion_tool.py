079add2593727ed1cc5175b5ddaaf6644a196077
Wanjiru-mugo <wanjirumugomary@gmail.com>
2025-08-07 01:00:53 +0300

    temp conversion calculator

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -2,12 +2,12 @@ FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9 #global var
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5 #global var
 
 def convert_to_celsius(fahrenheit):
-    farenheit = temp
-    return (farenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+    fahrenheit = temp
+    return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
 def convert_to_fahrenheit(celsius):
     celsius = temp
-    return (celsius + 32) * CELSIUS_TO_FAHRENHEIT_FACTOR
+    return celsius * CELSIUS_TO_FAHRENHEIT_FACTOR + 32
 
 temp = int(input('Enter the temperature to convert: '))
 ans = input('Is this temperature in Celsius or Farenheit? (C/F): ').upper().strip()
@@ -15,7 +15,7 @@ if ans == 'F':
     new_temp = convert_to_celsius(temp)
     print(new_temp)
 elif ans == 'C':
-    new_temp = convert_to_farenheit(temp)
+    new_temp = convert_to_fahrenheit(temp)
     print(new_temp)
 else:
     print('Invalid temperature. Please enter a numeric value.')
