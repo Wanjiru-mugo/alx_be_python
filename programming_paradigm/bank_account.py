46cca7714e2f15b11042f1e6ca007658b7d9e561
Wanjiru-mugo <wanjirumugomary@gmail.com>
2025-08-11 02:40:53 +0300

    banking operations

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,22 @@
+class BankAccount:
+    def __init__(self, account_balance):
+        self.account_balance = account_balance
+        self.initial_balance = 0
+    def deposit(self, amount):
+        amount += self.account_balance
+        self.account_balance = amount
+        return f"Deposited: ${amount}"
+    def withdraw(self, amount):
+        if not amount > self.account_balance:
+            self.account_balance -= amount
+            print(f"Withdrew: ${amount}")
+            return True
+        else:
+            print('Insufficient funds')
+            return False
+            
+    def display_balance(self):
+        print(f"Current balance: ${self.account_balance}")
+
+        
+
