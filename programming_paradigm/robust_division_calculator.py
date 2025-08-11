8ff48366638390babd8b3b7f9c6cee8b6f0a2f7c
Wanjiru-mugo <wanjirumugomary@gmail.com>
2025-08-11 04:56:56 +0300

    ZeroDivisionError

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -8,6 +8,8 @@ def safe_divide(numerator, denominator):
         else:
             result = numerator / denominator
             print(f"The result of the division is {result: .1f}")
+    except ZeroDivisionError:
+        return f"Error: Cannot divide by zero."
     except ValueError:
         if not isinstance(numerator, (int, float)) and isinstance(denominator, (int, float)):
             return f"Error: Please enter numeric values only."
