47d2d74ce4a774e4e583c5d137ca480a22199358
Wanjiru-mugo <wanjirumugomary@gmail.com>
2025-08-11 07:52:18 +0300

    lists in classes

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,23 +1,50 @@
 class Book:
-    def __init__(self, title, author, __is_checked_out):
+    def __init__(self, title, author):
         self.title = title
         self.author = author
-        self.__is_checked_out = __is_checked_out
+        self._is_checked_out = False #initialised private attribute
+
+    def check_out(self):
+        if not self._is_checked_out:
+            return True
+        else:
+            return False
+
+    def return_book(self):
+        if self._is_checked_out:
+            return True
+        else:
+            return False
+
+    def is_available(self):
+        return not self._is_checked_out
+
 
 class Library:
     def __init__(self):
-        self.__books = []
+        self._books = [] #initilised empty list
 
     def add_book(self):
-        self.__books.append(new_book)
+        self._books.append(book)
 
     def check_out_book(self, title):
-        self.__books.remove(title)
+        for book in self._books:
+            if book.title == title and book.is_available():
+                book.check_out()
+                return True
+            else:
+                return False
 
     def return_book(self, title):
-        self.__books.add(title)
-        return True
+        for book in self._books:
+            if book.title == title and not book.is_available():
+                book.return_book()
+                return True
+            else:
+                return False
 
     def list_available_books(self):
-        print(self.__books)
+        for book in self._books:
+            if book.is_available():
+                print(f"{book.title} by {book.author}")
 
