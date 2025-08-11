d3999dee48d5d5d6b9e3140335eb45fe721985b5
Wanjiru-mugo <wanjirumugomary@gmail.com>
2025-08-11 05:57:27 +0300

    lists in classes

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,22 @@
+class Book:
+    def __init__(self, title, author, __is_checked_out):
+        self.title = title
+        self.author = author
+        self.__is_checked_out = __is_checked_out
+
+class Library:
+    def __init__(self, __books):
+        self.__books = []
+
+    def add_book(self):
+        self.__books.append(new_book)
+
+    def check_out_book(self, title):
+        self.__books.remove(title)
+
+    def return_book(self, title):
+        self.__books.add(title)
+
+    def list_available_books(self):
+        print(self.__books)
+
